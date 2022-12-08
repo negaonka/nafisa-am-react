fdf48db414c1db9a01d3eb70b1fb2e7e3e710b13
negaonka <neha.n.gaonkar@gmail.com>
2022-12-08 19:21:34 +0000

    fixing the react scope

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -1,27 +1,30 @@
-const ProjectCard = (props) =>{
-
-
-    return <div>        <div className="job">
-    <div className="job-info">
-      <div className="job-img">
-        <a href={props.githubLink}>
-          {" "}
-          <img
-            src={props.imagePath}
-            className="my-2"
-            style={{ width: "100%" }}
-            alt="Code-Quiz"
-          />
-        </a>
+const ProjectCard = (props) => {
+  return (
+    <>
+      <div>
+        {" "}
+        <div className="job">
+          <div className="job-info">
+            <div className="job-img">
+              <a href={props.githubLink}>
+                {" "}
+                <img
+                  src={props.imagePath}
+                  className="my-2"
+                  style={{ width: "100%" }}
+                  alt="Code-Quiz"
+                />
+              </a>
+            </div>
+            <div className="job-text">
+              <h4>{props.title}</h4>
+              <p>{props.description}</p>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="job-text">
-        <h4>{props.title}</h4>
-        <p>
-          {props.description}
-        </p>
-      </div>
-    </div>
-  </div></div>
-}
+    </>
+  );
+};
 
-export default ProjectCard
+export default ProjectCard;
