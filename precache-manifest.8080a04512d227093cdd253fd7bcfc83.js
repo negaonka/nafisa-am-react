a1abf3d82c4e4c23fb3947f09fd6d6eac8ba53e4
negaonka <neha.n.gaonkar@gmail.com>
2022-12-08 19:27:20 +0000

    Updates

---
+++ precache-manifest.8080a04512d227093cdd253fd7bcfc83.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "d50bc61c861d197f2be4",
+    "url": "/nafisa-am-react/static/css/main.88c051e7.chunk.css"
+  },
+  {
+    "revision": "d50bc61c861d197f2be4",
+    "url": "/nafisa-am-react/static/js/main.aae3af10.chunk.js"
+  },
+  {
+    "revision": "15b74663c72ca596f435",
+    "url": "/nafisa-am-react/static/js/runtime~main.ffa86018.js"
+  },
+  {
+    "revision": "871e493b26563e6b1f2e",
+    "url": "/nafisa-am-react/static/js/2.7a790698.chunk.js"
+  },
+  {
+    "revision": "49acc14f549ae9bd76c488dcc970237e",
+    "url": "/nafisa-am-react/static/media/Profile_Pic.49acc14f.png"
+  },
+  {
+    "revision": "642037df3790fc0d16064c9d2b5662a8",
+    "url": "/nafisa-am-react/static/media/CodeQuiz.642037df.png"
+  },
+  {
+    "revision": "1624ce911aafa610c2435a9c69dd5da6",
+    "url": "/nafisa-am-react/static/media/LyricFinder.1624ce91.png"
+  },
+  {
+    "revision": "ffc1c8c0e1fc96e500471bde431affef",
+    "url": "/nafisa-am-react/static/media/designTeam.ffc1c8c0.png"
+  },
+  {
+    "revision": "104d8ed9b4be2fe14ffb90ab5cc3884e",
+    "url": "/nafisa-am-react/static/media/empTracker.104d8ed9.png"
+  },
+  {
+    "revision": "3ffbf2009a46540c07b9ba36a522c3e5",
+    "url": "/nafisa-am-react/static/media/scaCarRental.3ffbf200.png"
+  },
+  {
+    "revision": "4dd6aef3116234fbd39296fdb2c02644",
+    "url": "/nafisa-am-react/static/media/Pwa19.4dd6aef3.png"
+  },
+  {
+    "revision": "f07f766ecca51395aa6c573c21e19086",
+    "url": "/nafisa-am-react/static/media/N_Mohamed.f07f766e.pdf"
+  },
+  {
+    "revision": "2bf71f3866efd95e104649e80214d926",
+    "url": "/nafisa-am-react/static/media/cover.2bf71f38.jpg"
+  },
+  {
+    "revision": "c9295a593084f574752ab067e853760e",
+    "url": "/nafisa-am-react/index.html"
+  }
+];
