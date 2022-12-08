a9c7f830b052ff29ffefd340692ad1339458d2e3
nafisa-am <nafisa.am91@gmail.com>
2022-12-08 01:38:07 +0000

    minor changes in css

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,23 +1,23 @@
 import React, { useState } from "react";
 
-// Here we import a helper function that will check if the email is valid
+// Importing helper function that will check if the email is valid
 import { validateEmail } from "../../utils/helpers";
 
 function Contact() {
-  // Create state variables for the fields in the form
-  // We are also setting their initial values to an empty string
+  // Creates a state variable for the fields in the form
+  
   const [email, setEmail] = useState("");
   const [userName, setUserName] = useState("");
   const [message, setMessage] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
 
   const handleInputChange = (e) => {
-    // Getting the value and name of the input which triggered the change
+    // Getting the value and name of the input 
     const { target } = e;
     const inputType = target.name;
     const inputValue = target.value;
 
-    // Based on the input type, we set the state of either email, username, and message
+    // Based on the input type, we set the state for  email/username, and message
     if (inputType === "email") {
       setEmail(inputValue);
     } else if (inputType === "userName") {
@@ -28,15 +28,15 @@ function Contact() {
   };
 
   const handleFormSubmit = (e) => {
-    // Preventing the default behavior of the form submit (which is to refresh the page)
+    // Preventing the default behavior of the form submit (refreshing the page)
     e.preventDefault();
 
-    // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
+    // Email validator
     if (!validateEmail(email) || !userName) {
       setErrorMessage("Email or Name is invalid");
-      // We want to exit out of this code block if something is wrong so that the user can correct it
+     
       return;
-      // Then we check to see if the message is not valid. If so, we set an error message regarding the message.
+      // If email not valid this throws an error
     }
 
     if (!setMessage(message)) {
@@ -44,7 +44,7 @@ function Contact() {
       return;
     }
 
-    // If everything goes according to plan, we want to clear out the input after a successful submission.
+   
     setUserName("");
     setMessage("");
     setEmail("");
